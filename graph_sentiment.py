5f1f44400e8a4f26882be4d0daab1fbdeaa97c01
Ameya Shanbhag <ameya.shanbhag@gmail.com>
2017-02-21 02:38:26 +0530

    Initial commit

---
+++ graph_sentiment.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 21 02:25:33 2017
+
+@author: ameya.shanbhag
+"""
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+import time
+
+style.use("ggplot")
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+def animate(i):
+    pullData = open("twitter-out.txt","r").read()
+    lines = pullData.split('\n')
+
+    xar = []
+    yar = []
+
+    x = 0
+    y = 0
+
+    for l in lines[-200:]:
+        x += 1
+        if "pos" in l:
+            y += 1
+        elif "neg" in l:
+            y -= 1
+
+        xar.append(x)
+        yar.append(y)
+        
+    ax1.clear()
+    ax1.plot(xar,yar)
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+plt.show()
